94be6dbbc32471f7e5acb93517c600d0b503c2cb
Joselyn Huaman <j.devetrieva@gmail.com>
2025-02-18 20:11:15 -0500

    Proyecto-Peru1802

--- src/Pages/GastronomyPage/GastronomyPage.jsx
+++ src/Pages/GastronomyPage/GastronomyPage.jsx
@@ -1,27 +1,33 @@
+import './GastronomyPage.scss';
 
 function GastronomyPage (){
 
     return (
 
-        <div>
-            <div>
-                <div>
-                    <span>Peruvian Cuisine</span>
+        <div className='gastronomypage-padre'>
+            <div className='gastronomypage-padre_descripcion'>
+                <div className='gastronomypage-padre_descripcion_superior'>
+                    <span>
+                        Peruvian Cuisine
+                    </span>
+                </div >
+                <div className='gastronomypage-padre_descripcion_contenido'>
+                    <span>
+                    Traditional peruvian dish in coast
+                    </span>
+                    <h1>
+                    LOMO SALTADO
+                    </h1>
+                    <p>
+                    a stir fry that typically combines marinated strips of sirloin (or other beef steak) with onions, tomatoes, french fries, and other ingredients; and is typically served with rice
+                    </p>
                 </div>
                 <div>
 
                 </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
+                
             </div>
-            <div>
+            <div className='gastronomypage-padre_imagen'>
 
             </div>
         </div>
